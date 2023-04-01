9366d86a121e3a429bb164fd4d1f1b23ade53527
sami92news <sami.akram@92newshd.tv>
2023-04-01 15:51:10 +0500

    setup git

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-import setuptools
+from setuptools import find_namespace_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='admin_form_image_preivew',
-    version='0.0.23',
+setup(
+    name='admin-form-image-preivew',
+    version='0.0.12',
     description='Test package.',
-    py_modules=["admin_form_image_preivew"],
-    package_dir={'': 'src'},
-    packages = setuptools.find_packages(where="src"),
+    # package_dir={'': 'src'},
+    # packages = find_packages(where="src"),
+    packages=find_namespace_packages(
+        include=[
+            "src.app1.templates.admin",
+            "src.app1",
+        ],
+    ),
     classifiers=[
         "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
@@ -20,8 +25,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     author="Sami Akram",
     author_email="samiakram@live.com",
-    url="https://github.com/humblesami/admin_form_image_preivew",
-    # install_requires=['gitpython'],
+    url="https://github.com/humblesami/admin_form_image_preivew.git",
+    install_requires=['Django'],
     # keywords='git tag git-tagup tagup tag-up version autotag auto-tag commit message',
     # project_urls={'Homepage': 'https://initialcommit.com/tools/git-tagup',},
     # entry_points={ 'console_scripts': [ 'git-tagup=git_tagup.__main__:main','gtu=git_tagup.__main__:main',],}
