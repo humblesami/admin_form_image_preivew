6d98cbb2cdeebe268a10d224c4bb21496ae06591
sami92news <sami.akram@92newshd.tv>
2023-04-03 15:39:25 +0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,80 +1,71 @@
-import os
+import sys
 from setuptools import find_namespace_packages, setup
 
+def main(vv):
 
-install_requires = [
-    "django",
-]
+    install_requires = [
+        "django",
+    ]
 
-test_requires = [
-    "tox",
-    "tox-gh-actions",
-    "pluggy>=0.7",
-    "mock",
-    "unittest-xml-reporting",
-    "codacy-coverage",
-    "django-migration-fixer",
-]
+    deploy_requires = [
+        "bump2version",
+        "readme_renderer[md]",
+        "git-changelog",
+    ]
 
-deploy_requires = [
-    "bump2version",
-    "readme_renderer[md]",
-    "git-changelog",
-]
-
-
-local_dev_requires = [
-    "pip-tools",
-    "check-manifest",
-]
-
-extras_require = {
-    "development": [
-        local_dev_requires,
-        install_requires,
-        test_requires,
-        deploy_requires,
-    ],
-    "test": test_requires,
-    "deploy": deploy_requires,
-}
-
-BASE_DIR = os.path.dirname(__file__)
-README_PATH = os.path.join(BASE_DIR, "README.md")
-LONG_DESCRIPTION_TYPE = "text/markdown"
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name='admin-form-image-preivew',
-    version='1.3',
-    description='Test package.',
-    # cmdclass={'bdist_wheel': bdist_wheel},
-    # package_dir={'': 'src'},
-    # packages = find_packages(where="src"),
-
-    # packages = find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-    ],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Sami Akram",
-    author_email="samiakram@live.com",
-    url="https://github.com/humblesami/admin_form_image_preivew.git",
-
-    include_package_data=True,
-    python_requires=">=3.6",
-    install_requires=install_requires,
-    tests_require=["coverage"],
-    extras_require=extras_require,
-    packages=find_namespace_packages(
-        include=[
-            "app1.templates.admin",
-            "app1"
+    extras_require = {
+        "development": [
+            install_requires,
+            deploy_requires,
         ],
-    ),
-)
+        "deploy": deploy_requires,
+    }
+
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+
+    setup(
+        name='admin-form-image-preivew',
+        version=str(vv),
+        description='Test package.',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Sami Akram",
+        author_email="samiakram@live.com",
+        url="https://github.com/humblesami/admin_form_image_preivew.git",
+
+        python_requires=">=3.6",
+        install_requires=install_requires,
+        tests_require=["coverage"],
+        extras_require=extras_require,
+
+        include_package_data=True,
+        setup_requires=['setuptools_scm'],
+        packages=find_namespace_packages(
+            include=[
+                "app1.templates.admin",
+                "app1"
+            ],
+        ),
+    )
+
+if __name__ == '__main__':
+    version = ''
+    if sys.argv:
+        last_arg = sys.argv[-1:][0]
+        if last_arg.startswith('-v='):
+            sys.argv = sys.argv[:-1]
+            version = last_arg[3:]
+    try:
+        main(version)
+        print("Success")
+    except Exception as ex:
+        message = str(ex)
+        print("Error in set up ", message)
+    print("Complete")
